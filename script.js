a83900eedf09092127cd3ecd1d5f77c811cd6783
vignesh096 <123861333+vignesh096@users.noreply.github.com>
2023-04-09 10:45:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+const heartIcon = container.querySelector("img");
+container.addEventListener("dblclick", (e) => {
+  //calculate cooradinates for placing heart
+  let Xvalue = e.clientX - e.target.offsetLeft;
+  let Yvalue = e.clientY - e.target.offsetTop;
+  heartIcon.style.left = `${Xvalue}px`;
+  heartIcon.style.top = `${Yvalue}px`;
+
+  heartIcon.classList.add("dhak-dhak");
+  //remove animation class after 1 sec
+  setTimeout(() => {
+    heartIcon.classList.remove("dhak-dhak");
+
+    console.log("done");
+  }, 1000);
+});
